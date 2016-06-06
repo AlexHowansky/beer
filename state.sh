8ad1a0cde5e82f7e69f1199070c08a7f5ca3b9fa
Alex Howansky <alex.howansky@gmail.com>
2016-06-06 20:07:09 +0000

    Moved US files into a subdir.

--- state.sh
+++ state.sh
@@ -6,18 +6,23 @@ then
     exit 1
 fi
 
-if ! grep -q "${1}" states
+if ! grep -q "^${1}$" states
 then
-    echo "Error: Unknown state, please provide the full state name and not the abbreviation. Also make sure you quote names with spaces."
+    echo "Error: Unknown state, please provide the full state"
+    echo "       name and not the abbreviation. If the state name"
+    echo "       has spaces, make sure you quote the paramter."
+    echo
+    echo "Examples: ${0} Vermont"
+    echo "          ${0} \"New York\""
     exit 1
 fi
 
 STATE=$(echo ${1} | sed 's/ /_/g')
 
-mkdir -p "archive/${STATE}"
+mkdir -p "archive/United_States/${STATE}"
 
-FILE="archive/${STATE}/${STATE}_$(date +%Y%m%d).csv"
-LAST="archive/${STATE}/$(ls "archive/${STATE}" | sort -nr | head -1)"
+FILE="archive/United_States/${STATE}/${STATE}_$(date +%Y%m%d).csv"
+LAST="archive/United_States/${STATE}/$(ls "archive/United_States/${STATE}" | sort -nr | head -1)"
 
 curl --silent --data "action=get_breweries&search_by=statename&_id=${1}" "https://www.brewersassociation.org/wp-admin/admin-ajax.php" | ./extract.php > "${FILE}"
 
